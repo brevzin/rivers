2a5d4063d30b8918ba8d3e0ecb30e4bea4d7cfee
Barry Revzin <barry.revzin@gmail.com>
2021-11-30 10:15:55 -0600

    Adding concat benchmark.

--- bench/benchmark.cxx
+++ bench/benchmark.cxx
@@ -3,8 +3,10 @@
 #include <rivers/rivers.hpp>
 #include "nanobench.h"
 #include "flow.hpp"
-#include <range/v3/view/transform.hpp>
+#include <range/v3/view/concat.hpp>
 #include <range/v3/view/filter.hpp>
+#include <range/v3/view/take.hpp>
+#include <range/v3/view/transform.hpp>
 #include <range/v3/numeric/accumulate.hpp>
 
 int main() {
@@ -17,7 +19,6 @@ int main() {
     auto is_even = [](int x) { return x % 2 == 0; };
     auto triple = [](int x) { return 3 * x; };
 
-    bench.relative(true);
     bench.run("transform_filter_handwritten",
         [&]{
             int res = 0;
@@ -68,4 +69,57 @@ int main() {
                    .filter(is_even);
             an::doNotOptimizeAway(r.sum());
         });
+
+    auto moar_ints = bunch_of_ints;
+    std::reverse(moar_ints.begin(), moar_ints.end());
+
+    bench.run("concat_take_transform_filter_handwritten", [&]{
+        int res = 0;
+        int take = 1'500'000;
+        for (int i : bunch_of_ints) {
+            if (take) {
+                --take;
+                i = triple(i);
+                if (is_even(i)) {
+                    res += i;
+                }
+            }
+        }
+        for (int i : moar_ints) {
+            if (take) {
+                --take;
+                i = triple(i);
+                if (is_even(i)) {
+                    res += i;
+                }
+            }
+        }
+        an::doNotOptimizeAway(res);
+    });
+
+    bench.run("concat_take_transform_filter_rivers", [&]{
+        auto r = rvr::from_cpp(bunch_of_ints)
+               .chain(rvr::from_cpp(moar_ints))
+               .take(1'500'000)
+               .map(triple)
+               .filter(is_even);
+        an::doNotOptimizeAway(r.sum());
+    });
+
+    bench.run("concat_take_transform_filter_range-v3", [&]{
+        namespace rv = ranges::views;
+        auto r = rv::concat(bunch_of_ints, moar_ints)
+               | rv::take(1'500'000)
+               | rv::transform(triple)
+               | rv::filter(is_even);
+        an::doNotOptimizeAway(ranges::accumulate(r, 0));
+    });
+
+    bench.run("concat_take_transform_filter_flow", [&]{
+        auto r = flow::chain(flow::from(bunch_of_ints), flow::from(moar_ints))
+               .take(1'500'000)
+               .map(triple)
+               .filter(is_even);
+        an::doNotOptimizeAway(r.sum());
+    });
 }
